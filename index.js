20a3d63647b811c254af5bedeace48d0e11719d8
MORGANJASON <420morganjason@gmail.com>
2023-03-10 15:24:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName){
+    let messages = [];
+    for(let i=0; i < names.length; i++){
+         console.log(i);
+
+       console.log (messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));
+      debugger;
+    }
+    return messages;
+}
+writeCards( ["Ada", "Brendan", "Ali"],"birthday");
+
+function countDown(number){
+
+    // let num = number;
+    while(number >= 0){
+        console.log(number);
+        number --;
+    }
+    return number;
+}countdown
